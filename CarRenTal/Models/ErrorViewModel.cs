c6463ea1b03697da29ca48d192c1827937ce0a54
cuog1598 <cuog1598@gmail.com>
2019-04-18 20:13:30 +0700

    importting project

---
+++ CarRenTal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarRenTal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
